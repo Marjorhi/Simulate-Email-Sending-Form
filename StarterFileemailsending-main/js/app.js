27d10a13e1f8157b31c6a875ad33865c24d8c2d4
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-10-25 08:07:18 +0800

    Validating the Length of the Fields to app.js

--- StarterFileemailsending-main/js/app.js
+++ StarterFileemailsending-main/js/app.js
@@ -1,5 +1,8 @@
 //Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
+email = document.getElementById ('email'),
+subject = document.getElementById ('subject'),
+message = document.getElementById ('message');
 
 
 
@@ -9,6 +12,11 @@ eventListeners ();
 function eventListeners () {
     //App Init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    //Validate the Forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 
@@ -20,4 +28,22 @@ function eventListeners () {
 function appInit () {
     // Disable the send button on load
     sendBtn.disabled = true;
+}
+
+//Validate the Fields
+function validateField() {
+    let errors;
+
+    // Validate the Length of the Field
+    validateLength(this)
+}
+//Validate the Length of the Fields
+function validateLength (field) {
+    if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error')
+    }
 }
